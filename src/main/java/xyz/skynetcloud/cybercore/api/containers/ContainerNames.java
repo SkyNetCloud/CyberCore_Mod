c51463cea2eb8c6443ab63e3677259499a37f0b3
SkyNetCloud <mcboxer1201@gmail.com>
2020-01-08 14:31:07 -0500

    New naming and block files

---
+++ src/main/java/xyz/skynetcloud/cybercore/api/containers/ContainerNames.java
@@ -0,0 +1,8 @@
+package xyz.skynetcloud.cybercore.api.containers;
+
+public class ContainerNames {
+	
+	public static class ContainerNameInit {
+		
+	}
+}
---
+++ src/main/java/xyz/skynetcloud/cybercore/api/tileentity/TileEntityNames.java
@@ -0,0 +1,9 @@
+package xyz.skynetcloud.cybercore.api.tileentity;
+
+public class TileEntityNames {
+	
+	public static class TileEntityInit {
+		
+	}
+
+}
