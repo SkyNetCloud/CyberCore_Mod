813e4137c4afd0190fffb463b29da78e204c3aa7
SkyNetCloud <mcboxer1201@gmail.com>
2020-01-18 02:50:14 -0500

    Updated

---
+++ src/main/java/xyz/skynetcloud/cybercore/api/Names.java
@@ -0,0 +1,43 @@
+package xyz.skynetcloud.cybercore.api;
+
+import static xyz.skynetcloud.cybercore.CyberCoreMain.MODID;
+
+public class Names {
+
+	public static final String taco_name = (MODID + ":taco");
+	public static final String cheese_name = (MODID + ":cheese");
+	public static final String lettuce_name = (MODID + ":lettuce");
+	public static final String tomato_name = (MODID + ":tomato");
+
+	public static final String lettuce_seed_name = (MODID + ":lettuce_seed");
+	public static final String tomato_seed_name = (MODID + ":tomato_seed");
+
+	public static final String SPEED_LVL_1 = (MODID + ":speed_upgrade_lvl_1");
+	public static final String SPEED_LVL_2 = (MODID + ":speed_upgrade_lvl_2");
+	public static final String SPEED_LVL_3 = (MODID + ":speed_upgrade_lvl_3");
+	public static final String SPEED_LVL_4 = (MODID + ":speed_upgrade_lvl_4");
+	public static final String LUNAR_LVL_1 = (MODID + ":lunar_upgrade_lvl_1");
+	public static final String LUNAR_LVL_2 = (MODID + ":lunar_upgrade_lvl_2");
+	public static final String LUNAR_LVL_3 = (MODID + ":lunar_upgrade_lvl_3");
+	public static final String LUNAR_LVL_4 = (MODID + ":lunar_upgrade_lvl_4");
+	public static final String CYBER_INGOT = (MODID + ":cyber_ingot");
+	public static final String CYBER_AXE = (MODID + ":cyber_axe");
+	public static final String CYBER_PICKAXE = (MODID + ":cyber_pickaxe");
+	public static final String CYBER_SHOVEL = (MODID + ":cyber_shovel");
+	public static final String CYBER_SWORD = (MODID + ":cyber_sword");
+	public static final String CYBER_HOE = (MODID + ":cyber_hoe");
+
+	public static final String LUNARGEN_CON = (MODID + ":lunar_gen_machine_con");
+	public static final String POWER_BOX_CON = (MODID + ":power_box_con");
+	public static final String POWERED_FURNACE_CON = (MODID + ":poweredfurnace_con");
+
+	public static final String cyber_slab = (MODID + ":cyber_slab");
+	public static final String cyber_stairs = (MODID + ":cyber_stairs");
+
+	public static final String lunar = (MODID + ":lunarsolargenerator_block");
+	public static final String cable = (MODID + ":cable");
+	public static final String power_box = (MODID + ":power_storage");
+	public static final String dimWorldLink = (MODID + ":dimworldlink");
+	public static final String cyberore = (MODID + ":cyber_ore");
+
+}
