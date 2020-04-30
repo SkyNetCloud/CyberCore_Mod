88e8bd9e7cb42125b9dc5d865fcceb16713b199a
SkyNetCloud <SkyNetCloud@DESKTOP-M7TECBC>
2020-04-30 03:16:25 -0400

    Lab Structure Info Plus Loot Table for chest

---
+++ src/main/java/xyz/skynetcloud/cybercore/world/gen/feature/structure/Lab.java
@@ -0,0 +1,107 @@
+package xyz.skynetcloud.cybercore.world.gen.feature.structure;
+
+import java.util.Random;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.ChestTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.feature.structure.TemplateStructurePiece;
+import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
+import net.minecraft.world.gen.feature.template.JigsawReplacementStructureProcessor;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import xyz.skynetcloud.cybercore.CyberCoreMain;
+import xyz.skynetcloud.cybercore.init.InitStructurePieceType;
+import xyz.skynetcloud.cybercore.world.gen.feature.LabConfig;
+
+public class Lab {
+	public static final ResourceLocation Lab_Build_ONE = new ResourceLocation(CyberCoreMain.MODID, "lab_build");
+
+	// public static final ResourceLocation Lab_Build_Mark_2 = new
+	// ResourceLocation(CyberCoreMain.MODID, "lab_build_mark_2");
+
+	public static class Piece extends TemplateStructurePiece {
+
+		public static final ResourceLocation LAB_CHEST_LOOT = new ResourceLocation(CyberCoreMain.MODID,
+				"chests/lab_chest");
+
+		private Rotation rotation;
+		private ResourceLocation templateLocation;
+
+		public Piece(TemplateManager manager, BlockPos pos, Rotation rotation, LabConfig config) {
+			super(InitStructurePieceType.LOOTABLE_lab, 0);
+			this.rotation = rotation;
+			this.templatePosition = pos;
+			this.templateLocation = config.template;
+			this.loadTemplate(manager);
+		}
+
+		public Piece(TemplateManager manager, CompoundNBT compound) {
+			super(InitStructurePieceType.LOOTABLE_lab, compound);
+			this.rotation = Rotation.valueOf(compound.getString("Rot"));
+			this.templateLocation = new ResourceLocation(compound.getString("Template"));
+			this.loadTemplate(manager);
+		}
+
+		private void loadTemplate(TemplateManager manager) {
+			Template template = manager.getTemplateDefaulted(this.templateLocation);
+			PlacementSettings settings = (new PlacementSettings()).setRotation(this.rotation)
+					.setCenterOffset(new BlockPos(5, 0, 5)).addProcessor(BlockIgnoreStructureProcessor.STRUCTURE_BLOCK)
+					.addProcessor(JigsawReplacementStructureProcessor.INSTANCE);
+			this.setup(template, this.templatePosition, settings);
+		}
+
+		@Override
+		protected void readAdditional(CompoundNBT compound) {
+			super.readAdditional(compound);
+			compound.putString("Template", this.templateLocation.toString());
+			compound.putString("Rot", this.rotation.name());
+		}
+
+		@Override
+		protected void handleDataMarker(String function, BlockPos pos, IWorld worldIn, Random rand,
+				MutableBoundingBox sbb) {
+			if ("chest".equals(function)) {
+				worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+				TileEntity tileentity = worldIn.getTileEntity(pos.down());
+				if (tileentity instanceof ChestTileEntity) {
+					((ChestTileEntity) tileentity).setLootTable(LAB_CHEST_LOOT, rand.nextLong());
+				}
+			}
+		}
+
+		@Override
+		public boolean func_225577_a_(IWorld world, ChunkGenerator<?> generator, Random rand, MutableBoundingBox bounds,
+				ChunkPos chunkPos) {
+			int posY = world.getHeight(Heightmap.Type.WORLD_SURFACE_WG, this.templatePosition.getX(),
+					this.templatePosition.getZ()) - 1;
+			this.templatePosition = new BlockPos(this.templatePosition.getX(), posY, this.templatePosition.getZ());
+
+			switch (this.rotation) {
+			case CLOCKWISE_90:
+				this.templatePosition = this.templatePosition.add(-1, 0, 0);
+				break;
+			case CLOCKWISE_180:
+				this.templatePosition = this.templatePosition.add(-1, 0, -1);
+				break;
+			case COUNTERCLOCKWISE_90:
+				this.templatePosition = this.templatePosition.add(0, 0, -1);
+				break;
+			default:
+				break;
+			}
+
+			return super.func_225577_a_(world, generator, rand, bounds, chunkPos);
+		}
+	}
+}
