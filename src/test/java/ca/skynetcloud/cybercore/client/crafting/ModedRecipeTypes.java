f5229c085d7ab76c5743b83f297028a0e07f3063
SkyNetCloud <mcboxer1201@gmail.com>
2021-12-12 05:11:30 -0500

    Removed The Test Folder

--- src/test/java/ca/skynetcloud/cybercore/client/crafting/ModedRecipeTypes.java
+++
@@ -1,8 +0,0 @@
-package ca.skynetcloud.cybercore.client.crafting;
-
-import ca.skynetcloud.cybercore.client.crafting.recipeclasses.ColorChangerRecipe;
-import net.minecraft.world.item.crafting.RecipeType;
-
-public final class ModedRecipeTypes {
-	public static final RecipeType<ColorChangerRecipe> COLORING = RecipeType.register("cybercore:coloring");
-}
